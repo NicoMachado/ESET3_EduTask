198995cad02a6ae05b7f3910cc58bb6ef90d6b42
marcossincovich1 <marcossincovich20@gmail.com>
2023-11-07 21:30:13 -0300

    index

--- src/views/indexView.php
+++ src/views/indexView.php
@@ -6,6 +6,30 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <header class="header">
+        <a href="#"><img class="logo" src="img/cheque.png" alt="logo"></a>
+        <nav class="nav">
+            <ul class="nav_ul">
+                <li class="nav_li"><a href="#">Contacto</a></li>
+                <li class="nav_li"><a href="#">EEST3</a></li>
+                <li class="nav_li"><a href="#">Nosotros</a></li>
+            </ul>
+        </nav>
+        <button class="nav_button" type="submit">LOGIN</button>
+    </header>
+    <main>
+        <div class="contain_slogan">
+            <span class="slogan">Planifica y perfeccciona</span>
+            <span class="slogan">tu vida con</span>
+            <span class="slogan">Edu Task</span>
+        </div>
+        <div class="contain_semi_slogan">
+            <p class="slogan_content">La organizacion es la clave para el exito, con Edu Task, te proporcionamos las herramientas para un camino mas claro hacia tus metas</p>
+            <button class="slogan_button" type="submit">Pruebala Gratis</button>
+        </div>
+    </main>
+    <aside class="aside">
+        
+    </aside>
 </body>
 </html>
